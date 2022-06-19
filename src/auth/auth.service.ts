f9cd4a57da3004de4c629334c1d78af1dbc7d6d4
farrukh zeb <farrukhxeb@gmail.com>
2022-06-19 14:35:28 +0500

    Hashed the password and updated the migrations with relations

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
   constructor(private prismaService: PrismaService) {}
-  signup(dto: AuthDto) {
-    return { msg: 'I have signed up' };
+  async signup(dto: AuthDto) {
+    // generate the password
+    const hash = await argon.hash(dto.password);
+    // save the new user in the db and return it
+    try {
+      const user = await this.prismaService.user.create({
+        data: {
+          email: dto.email,
+          password: hash,
+        },
+      });
+      delete user.password;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
   signin() {
     return { msg: 'I have signed in' };
